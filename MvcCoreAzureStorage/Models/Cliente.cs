ed2bb53a56c44c02b625efccb13d61a4aa48de39
serraguti <pacoserranox@gmail.com>
2026-04-13 14:11:33 +0200

    lunes

---
+++ MvcCoreAzureStorage/Models/Cliente.cs
@@ -0,0 +1,44 @@
+﻿using Azure;
+using Azure.Data.Tables;
+using System.Text.RegularExpressions;
+
+namespace MvcCoreAzureStorage.Models
+{
+    public class Cliente : ITableEntity
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public int Salario { get; set; }
+
+        //ID CLIENTE: ROW KEY
+        //CUANDO EL USUARIO ALMACENE UN ID DE CLIENTE, 
+        //NOSOTROS ALMACENAMOS ROWKEY
+        private int _IdCliente;
+        public int IdCliente
+        {
+            get { return this._IdCliente; }
+            set {
+                this._IdCliente = value;
+                this.RowKey = value.ToString();
+            }
+        }
+
+        //EMPRESA: PARTITION KEY
+        //CUANDO EL USUARIO ALMACENE UNA EMPRESA, 
+        //ALMACENAMOS PARTITION KEY
+        private string _Empresa;
+        public string Empresa
+        {
+            get { return this._Empresa; }
+            set {
+                this._Empresa = value;
+                this.PartitionKey = value;
+            }
+        }
+
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+        public DateTimeOffset? Timestamp { get; set; }
+        public ETag ETag { get; set; }
+    }
+}
