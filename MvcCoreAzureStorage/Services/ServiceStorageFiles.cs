2c58d0c3d40d1143f1a0cde9db540cc7ba378911
serraguti <pacoserranox@gmail.com>
2026-04-13 10:20:07 +0200

    Add project files.

---
+++ MvcCoreAzureStorage/Services/ServiceStorageFiles.cs
@@ -0,0 +1,69 @@
+﻿using Azure.Storage.Files.Shares;
+using Azure.Storage.Files.Shares.Models;
+
+namespace MvcCoreAzureStorage.Services
+{
+    public class ServiceStorageFiles
+    {
+        //TODO SERVICIO STORAGE TRABAJA SIEMPRE CON UN CLIENT
+        //DICHO CLIENTE PUEDE SER DIRECTAMENTE UN SHARED O 
+        //PODRIA SER EL SERVICIO DE FILES
+        private ShareDirectoryClient root;
+
+        public ServiceStorageFiles(IConfiguration configuration)
+        {
+            string azureKeys =
+                configuration.GetValue<string>
+                ("AzureKeys:StorageAccount");
+            //NUESTRO CLIENTE TRABAJARA SOBRE EL SHARED
+            //QUE HEMOS CREADO PREVIAMENTE (ejemplo)
+            ShareClient client =
+                new ShareClient(azureKeys, "ejemplo");
+            this.root = client.GetRootDirectoryClient();
+        }
+
+        //LISTAR TODOS LOS FICHEROS
+        public async Task<List<string>> GetFilesAsync()
+        {
+            List<string> files = new List<string>();
+            await foreach (ShareFileItem item in
+                this.root.GetFilesAndDirectoriesAsync())
+            {
+                files.Add(item.Name);
+            }
+            return files;
+        }
+
+        //LEER CONTENIDO DE UN FICHERO
+        public async Task<string> ReadFileAsync(string fileName)
+        {
+            ShareFileClient fileClient =
+                this.root.GetFileClient(fileName);
+            ShareFileDownloadInfo data =
+                await fileClient.DownloadAsync();
+            Stream stream = data.Content;
+            string contenido = "";
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                contenido = await reader.ReadToEndAsync();
+            }
+            return contenido;
+        }
+
+        public async Task 
+            UploadFileAsync(string fileName, Stream stream)
+        {
+            ShareFileClient fileClient =
+                this.root.GetFileClient(fileName);
+            await fileClient.CreateAsync(stream.Length);
+            await fileClient.UploadAsync(stream);
+        }
+
+        public async Task DeleteFileAsync(string fileName)
+        {
+            ShareFileClient fileClient =
+                this.root.GetFileClient(fileName);
+            await fileClient.DeleteAsync();
+        }
+    }
+}
